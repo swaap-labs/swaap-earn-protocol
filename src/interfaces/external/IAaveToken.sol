9120be6da9106f3fc506775a6e519c1a8e708df3
Christian Dahdah <christiandahdah@gmail.com>
2024-02-04 21:17:16 +0100

    fixed imported interfaces

--- src/interfaces/external/IAaveToken.sol
+++ src/interfaces/external/IAaveToken.sol
@@ -3,6 +3,6 @@ pragma solidity 0.8.21;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IAaveToken {
+interface IAaveToken is IERC20 {
     function UNDERLYING_ASSET_ADDRESS() external view returns (address);
 }
