b9f8b193317722d82fcedac3a8c8e12d4fc36447
David Bouba <d.m.bouba@gmail.com>
2024-02-26 11:38:07 +0100

    hartdhat: upgraded version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -105,7 +105,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.16",
+        version: "0.8.21",
         settings: {
           optimizer: {
             enabled: optimizerEnabled,
